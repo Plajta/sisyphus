fb18fe7088cc8a9e8300a74f232984424237b1d1
Viktor Suchý <susenky.opavia@gmail.com>
2025-04-14 13:02:01 +0200

    renderovat spravne sidebar

--- web/components/nav-main.tsx
+++ web/components/nav-main.tsx
@@ -19,6 +19,7 @@ import {
 	SidebarMenuSubButton,
 	SidebarMenuSubItem,
 } from "~/components/ui/sidebar";
+import { useDeviceStore } from "~/store/useDeviceStore";
 
 export interface NavItem {
 	title: string;
@@ -32,59 +33,68 @@ export function NavMain({ items }: { items: NavItem[] }) {
 	const pathname = usePathname();
 	const params = useParams();
 
+	const { connected } = useDeviceStore();
+
 	return (
 		<SidebarGroup>
 			<div className="flex flex-col gap-1">
 				<Serial />
 
-				<Link className="mb-2" href={`/dashboard/configuration/sheet/${params.sheetId}/dialog/sheet/new`}>
-					<Button className="w-full" variant="outline">
-						Přidat Konfiguraci
-					</Button>
-				</Link>
+				{connected && (
+					<Link className="mb-2" href={`/dashboard/configuration/sheet/${params.sheetId}/dialog/sheet/new`}>
+						<Button className="w-full" variant="outline">
+							Přidat Konfiguraci
+						</Button>
+					</Link>
+				)}
 			</div>
 
-			<SidebarMenu>
-				{items.map((item) => (
-					<Collapsible
-						key={item.title}
-						asChild
-						defaultOpen={item.url === pathname.replace(`/${params.sheetId}`, "")}
-					>
-						<SidebarMenuItem>
-							<SidebarMenuButton asChild tooltip={item.title}>
-								<a href={item.url}>
-									<item.icon />
-									<span>{item.title}</span>
-								</a>
-							</SidebarMenuButton>
-							{item.subItems?.length ? (
-								<>
-									<CollapsibleTrigger asChild>
-										<SidebarMenuAction className="data-[state=open]:rotate-90">
-											<ChevronRight />
-											<span className="sr-only">Toggle</span>
-										</SidebarMenuAction>
-									</CollapsibleTrigger>
-									<CollapsibleContent>
-										<SidebarMenuSub>
-											{item.subItems?.map((subItem) => (
-												<SidebarMenuSubItem key={subItem.title}>
-													<SidebarMenuSubButton asChild isActive={pathname === subItem.url}>
-														<a href={subItem.url}>
-															<span>{subItem.title}</span>
-														</a>
-													</SidebarMenuSubButton>
-												</SidebarMenuSubItem>
-											))}
-										</SidebarMenuSub>
-									</CollapsibleContent>
-								</>
-							) : null}
-						</SidebarMenuItem>
-					</Collapsible>
-				))}
-			</SidebarMenu>
+			{connected && (
+				<SidebarMenu>
+					{items.map((item) => (
+						<Collapsible
+							key={item.title}
+							asChild
+							defaultOpen={item.url === pathname.replace(`/${params.sheetId}`, "")}
+						>
+							<SidebarMenuItem>
+								<SidebarMenuButton asChild tooltip={item.title}>
+									<a href={item.url}>
+										<item.icon />
+										<span>{item.title}</span>
+									</a>
+								</SidebarMenuButton>
+								{item.subItems?.length ? (
+									<>
+										<CollapsibleTrigger asChild>
+											<SidebarMenuAction className="data-[state=open]:rotate-90">
+												<ChevronRight />
+												<span className="sr-only">Toggle</span>
+											</SidebarMenuAction>
+										</CollapsibleTrigger>
+										<CollapsibleContent>
+											<SidebarMenuSub>
+												{item.subItems?.map((subItem) => (
+													<SidebarMenuSubItem key={subItem.title}>
+														<SidebarMenuSubButton
+															asChild
+															isActive={pathname === subItem.url}
+														>
+															<a href={subItem.url}>
+																<span>{subItem.title}</span>
+															</a>
+														</SidebarMenuSubButton>
+													</SidebarMenuSubItem>
+												))}
+											</SidebarMenuSub>
+										</CollapsibleContent>
+									</>
+								) : null}
+							</SidebarMenuItem>
+						</Collapsible>
+					))}
+				</SidebarMenu>
+			)}
 		</SidebarGroup>
 	);
 }
