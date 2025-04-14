beb0b26d469e58172513224ce39033567a38413a
Viktor Suchý <susenky.opavia@gmail.com>
2025-04-14 12:50:24 +0200

    defaulni stranka kdyz neni nic pripojeny

--- web/app/(dashboard)/page.tsx
+++ web/app/(dashboard)/page.tsx
@@ -2,8 +2,13 @@
 
 export default function Page() {
 	return (
-		<div className="flex flex-1 flex-col gap-4 px-4 py-10">
-			<p>stranka</p>
+		<div className="flex flex-1 flex-col gap-4 px-4 py-10 justify-center items-center">
+			<div className="flex flex-col">
+				<p className="text-xl font-bold text-center">Komunikátor není připojen.</p>
+				<p className="text-muted-foreground text-sm">
+					Pro konfiguraci komuníkátoru prosím ho prosím připojte do USB díry, díks
+				</p>
+			</div>
 		</div>
 	);
 }
