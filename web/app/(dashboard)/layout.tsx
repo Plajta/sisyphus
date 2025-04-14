827194a0df409ec5ebb9e04964e35ea4d632477f
Viktor Suchý <susenky.opavia@gmail.com>
2025-04-14 12:45:34 +0200

    smazat a vypnout serverove casti

--- web/app/(dashboard)/layout.tsx
+++ web/app/(dashboard)/layout.tsx
@@ -1,24 +1,15 @@
+"use client";
+
 import { AppSidebar } from "~/components/app-sidebar";
 
 import { PageBreadcrumb } from "~/components/page-breadcrumb";
 import { Separator } from "~/components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
-import { prisma } from "~/modules/prisma";
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
-	const sheets = await prisma.sheet.findMany({
-		select: {
-			id: true,
-			name: true,
-		},
-		orderBy: {
-			createdAt: "asc",
-		},
-	});
-
+export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<SidebarProvider>
-			<AppSidebar sheets={sheets} />
+			<AppSidebar sheets={[]} />
 			<SidebarInset>
 				<header className="flex h-14 shrink-0 items-center gap-2">
 					<div className="flex flex-1 items-center gap-2 px-3">
@@ -26,9 +17,6 @@ export default async function Layout({ children }: { children: React.ReactNode }
 						<Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
 						<PageBreadcrumb />
 					</div>
-					{/* <div className="ml-auto px-3">
-						<NavActions />
-					</div> */}
 				</header>
 				{children}
 			</SidebarInset>
