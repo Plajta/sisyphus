2292adba3d869f5937c75cd328ee695dee5a7ac2
Viktor Suchý <susenky.opavia@gmail.com>
2025-04-14 12:32:54 +0200

    exportovat staticke html

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "img.privezemenakup.cz",
-			},
-		],
-	},
-	experimental: {
-		serverActions: {
-			bodySizeLimit: "10mb",
-		},
-	},
+	output: "export",
 };
 
 export default nextConfig;
