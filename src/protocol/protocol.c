ba43ee552d53758c4e66ca8fe5886e75dbf2a26f
Vesek <venda.straka@gmail.com>
2025-07-23 14:41:46 +0200

    Clean up protocol.c

--- src/protocol/protocol.c
+++ src/protocol/protocol.c
@@ -15,6 +15,8 @@
 #include "audio.h"
 #include "color.h"
 
+extern lfs_t lfs;
+
 void print_newline(char* c) {
     tud_cdc_write_str(c);
     tud_cdc_write_str("\r\n");
@@ -47,18 +49,18 @@ static bool wait_for_ack_with_timeout(uint32_t timeout_ms) {
     return false; // Timeout
 }
 
-void ls_command(lfs_t *lfs){
+void ls_command(){
     lfs_dir_t dir;
     struct lfs_info info;
 
     // Open the root directory
-    if (lfs_dir_open(lfs, &dir, "/") != LFS_ERR_OK) {
+    if (lfs_dir_open(&lfs, &dir, "/") != LFS_ERR_OK) {
         print_newline("err cannot open root directory");
         return;
     }
 
     // Read entries
-    while (lfs_dir_read(lfs, &dir, &info) > 0) {
+    while (lfs_dir_read(&lfs, &dir, &info) > 0) {
         char entry_info[64];
         if (info.type == LFS_TYPE_REG) {
             snprintf(entry_info, sizeof(entry_info), "%s f %u", info.name, info.size);
@@ -70,38 +72,38 @@ void ls_command(lfs_t *lfs){
     tud_cdc_write_char(EOT_CHAR);
 
     // Close the directory
-    lfs_dir_close(lfs, &dir);
+    lfs_dir_close(&lfs, &dir);
 }
 
-void pull_command(lfs_t *lfs, char *filename){
+void pull_command(char *filename){
     // yes this function reads the file two times, but for the CRC32 it's worth it
 
     // Open the file
     lfs_file_t file;
-    if (lfs_file_open(lfs, &file, filename, LFS_O_RDONLY) != LFS_ERR_OK) {
+    if (lfs_file_open(&lfs, &file, filename, LFS_O_RDONLY) != LFS_ERR_OK) {
         print_newline("err cannot open file");
         return;
     }
 
-    int32_t file_size = lfs_file_size(lfs, &file);
+    int32_t file_size = lfs_file_size(&lfs, &file);
 
     uint8_t crc_buffer[CRC_BUF_SIZE];
     uint32_t crc = 0xFFFFFFFF; // Initial CRC value
 
     lfs_ssize_t bytes_read;
-    while ((bytes_read = lfs_file_read(lfs, &file, crc_buffer, sizeof(crc_buffer))) > 0) {
+    while ((bytes_read = lfs_file_read(&lfs, &file, crc_buffer, sizeof(crc_buffer))) > 0) {
         crc = lfs_crc(crc, crc_buffer, bytes_read);
     }
 
     if (bytes_read < 0) {
         print_newline("err calculate crc");
-        lfs_file_close(lfs, &file);
+        lfs_file_close(&lfs, &file);
         return;
     }
 
     crc ^= 0xFFFFFFFF; // Finalize CRC value
 
-    lfs_file_rewind(lfs, &file);
+    lfs_file_rewind(&lfs, &file);
 
     char ack_buf[64];
     snprintf(ack_buf, sizeof(ack_buf), "ack %u %u", file_size, crc);
@@ -111,7 +113,7 @@ void pull_command(lfs_t *lfs, char *filename){
     // Read the file
     char buffer[MAX_BUF_SIZE];
     size_t read_size;
-    while ((read_size = lfs_file_read(lfs, &file, buffer, sizeof(buffer))) > 0) {
+    while ((read_size = lfs_file_read(&lfs, &file, buffer, sizeof(buffer))) > 0) {
         if (!wait_for_ack_with_timeout(TIMEOUT_MS)) {
             print_newline("err timeout");
             break;
@@ -121,14 +123,14 @@ void pull_command(lfs_t *lfs, char *filename){
     }
 
     // Close the file
-    lfs_file_close(lfs, &file);
+    lfs_file_close(&lfs, &file);
 }
 
-void push_command(lfs_t *lfs, const char *filename, uint32_t *size, uint32_t *expected_checksum) {
+void push_command(const char *filename, uint32_t *size, uint32_t *expected_checksum) {
     lfs_file_t file;
 
     // Open (or create) the file with write access
-    if(lfs_file_open(lfs, &file, filename, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC) != LFS_ERR_OK){
+    if(lfs_file_open(&lfs, &file, filename, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC) != LFS_ERR_OK){
         print_newline("err open file");
     }
 
@@ -158,7 +160,7 @@ void push_command(lfs_t *lfs, const char *filename, uint32_t *size, uint32_t *ex
                 sleep_ms(1);
                 if (absolute_time_diff_us(get_absolute_time(), timeout) < 0) {
                     print_newline("err timeout");
-                    lfs_file_close(lfs, &file);
+                    lfs_file_close(&lfs, &file);
                     return;
                 }
             }
@@ -168,16 +170,16 @@ void push_command(lfs_t *lfs, const char *filename, uint32_t *size, uint32_t *ex
         crc = lfs_crc(crc, buffer, to_write);
 
         // Write the chunk
-        int written = lfs_file_write(lfs, &file, buffer, to_write);
+        int written = lfs_file_write(&lfs, &file, buffer, to_write);
         if (written < 0) {
-            lfs_file_close(lfs, &file);
+            lfs_file_close(&lfs, &file);
             print_newline("err write file");
             return;
         }
 
         total_written += written;
     }
-    lfs_file_close(lfs, &file);
+    lfs_file_close(&lfs, &file);
 
     crc ^= 0xFFFFFFFF; // Finalize CRC value
 
@@ -186,54 +188,54 @@ void push_command(lfs_t *lfs, const char *filename, uint32_t *size, uint32_t *ex
     }
     else{
         print_newline("err checksum mismatch");
-        lfs_remove(lfs, filename);
+        lfs_remove(&lfs, filename);
     }
 }
 
-void rm_command(lfs_t *lfs, char *filename) {
+void rm_command(char *filename) {
     struct lfs_info info;
 
     // Check if the file exists
-    if (lfs_stat(lfs, filename, &info) < LFS_ERR_OK) {
+    if (lfs_stat(&lfs, filename, &info) < LFS_ERR_OK) {
         print_newline("err file not found");
         return;
     }
 
     // Delete the file
-    if (lfs_remove(lfs, filename) < LFS_ERR_OK) {
+    if (lfs_remove(&lfs, filename) < LFS_ERR_OK) {
         print_newline("err delete file");
     }
 
     print_newline("ack");
 }
 
-void mv_command(lfs_t *lfs, char *old_filename, char *new_filename) {
+void mv_command(char *old_filename, char *new_filename) {
     struct lfs_info info;
 
     // Check if the old file exists
-    if (lfs_stat(lfs, old_filename, &info) < LFS_ERR_OK) {
+    if (lfs_stat(&lfs, old_filename, &info) < LFS_ERR_OK) {
         print_newline("err file not found");
         return;
     }
 
     // Check if the destination file exists
-    if (lfs_stat(lfs, new_filename, &info) == LFS_ERR_OK) {
+    if (lfs_stat(&lfs, new_filename, &info) == LFS_ERR_OK) {
         print_newline("err destination exists");
         return;
     }
 
     // Rename the file
-    if (lfs_rename(lfs, old_filename, new_filename) < LFS_ERR_OK) {
+    if (lfs_rename(&lfs, old_filename, new_filename) < LFS_ERR_OK) {
         print_newline("err rename file");
     }
 
     print_newline("ack");
 }
 
-void info_command(lfs_t *lfs){
-    const struct lfs_config *used_lfs_config = lfs->cfg;
+void info_command(){
+    const struct lfs_config *used_lfs_config = lfs.cfg;
     char info_buffer[128];
-    snprintf(info_buffer, sizeof(info_buffer), "sisyphus %s %s %s %s %u %u %u %b", DEVICE_NAME, GIT_COMMIT_SHA, PROTOCOL_VERSION, BUILD_DATE, used_lfs_config->block_count, lfs_fs_size(lfs), used_lfs_config->block_size, USE_ETERNITY);
+    snprintf(info_buffer, sizeof(info_buffer), "sisyphus %s %s %s %s %u %u %u %b", DEVICE_NAME, GIT_COMMIT_SHA, PROTOCOL_VERSION, BUILD_DATE, used_lfs_config->block_count, lfs_fs_size(&lfs), used_lfs_config->block_size, USE_ETERNITY);
     print_newline(info_buffer);
 }
 
@@ -245,11 +247,11 @@ void measure_command(){
     print_newline(measure_buffer);
 }
 
-void play_command(lfs_t *lfs, char *filename) {
+void play_command(char *filename) {
     struct lfs_info info;
 
     // Check if the file exists
-    if (lfs_stat(lfs, filename, &info) < LFS_ERR_OK) {
+    if (lfs_stat(&lfs, filename, &info) < LFS_ERR_OK) {
         print_newline("err file not found");
         return;
     }
@@ -259,8 +261,8 @@ void play_command(lfs_t *lfs, char *filename) {
     play_audio(filename);
 }
 
-void reset_command(lfs_t *lfs) {
-    lfs_unmount(lfs);
+void reset_command() {
+    lfs_unmount(&lfs);
     #if USE_ETERNITY
         watchdog_reboot(0, 0, 0); // reboot to eternity
     #else
@@ -269,7 +271,7 @@ void reset_command(lfs_t *lfs) {
     while (1) tight_loop_contents(); // Just in case
 }
 
-void handle_command(lfs_t *lfs, char *cmd) {
+void handle_command(char *cmd) {
     char *args[MAX_ARG_COUNT];
     int argc = 0;
 
@@ -282,7 +284,7 @@ void handle_command(lfs_t *lfs, char *cmd) {
     if (argc == 0) return;
 
     if (strcmp(args[0], "ls") == 0) {
-        ls_command(lfs);
+        ls_command();
     }
     else if (strcmp(args[0], "pull") == 0)
     {
@@ -291,7 +293,7 @@ void handle_command(lfs_t *lfs, char *cmd) {
             return;
         }
 
-        pull_command(lfs, args[1]);
+        pull_command(args[1]);
     }
     else if (strcmp(args[0], "push") == 0)
     {
@@ -304,7 +306,7 @@ void handle_command(lfs_t *lfs, char *cmd) {
         uint32_t size = (uint32_t)strtoul(args[2], NULL, 10);
         uint32_t crc = (uint32_t)strtoul(args[3], NULL, 10);
 
-        push_command(lfs, filename, &size, &crc);
+        push_command(filename, &size, &crc);
     }
     else if (strcmp(args[0], "rm") == 0)
     {
@@ -313,7 +315,7 @@ void handle_command(lfs_t *lfs, char *cmd) {
             return;
         }
 
-        rm_command(lfs, args[1]);
+        rm_command(args[1]);
     }
     else if (strcmp(args[0], "mv") == 0)
     {
@@ -322,7 +324,7 @@ void handle_command(lfs_t *lfs, char *cmd) {
             return;
         }
 
-        mv_command(lfs, args[1], args[2]);
+        mv_command(args[1], args[2]);
     }
     else if (strcmp(args[0], "play") == 0)
     {
@@ -331,11 +333,11 @@ void handle_command(lfs_t *lfs, char *cmd) {
             return;
         }
 
-        play_command(lfs, args[1]);
+        play_command(args[1]);
     }
     else if (strcmp(args[0], "info") == 0)
     {
-        info_command(lfs);
+        info_command();
     }
     else if (strcmp(args[0], "measure") == 0)
     {
@@ -344,7 +346,7 @@ void handle_command(lfs_t *lfs, char *cmd) {
     else if (strcmp(args[0], "reset") == 0)
     {
         if (argc == 1) {
-            reset_command(lfs);
+            reset_command();
         }
         else{
             print_newline("err invalid arguments");
@@ -358,7 +360,7 @@ void handle_command(lfs_t *lfs, char *cmd) {
     tud_cdc_write_flush();
 }
 
-void protocol_loop(lfs_t *lfs)
+void protocol_loop()
 {
     char cmd_buf[CMD_BUF_SIZE];
     size_t cmd_len = 0;
@@ -380,7 +382,7 @@ void protocol_loop(lfs_t *lfs)
             for (size_t i = cmd_len-n; i < cmd_len; ++i) {
                 if (cmd_buf[i] == EOT_CHAR) { // 4 is EOT
                     cmd_buf[i] = '\0';  // Null-terminate line
-                    handle_command(lfs, cmd_buf);  // Process command
+                    handle_command(cmd_buf);  // Process command
 
                     cmd_len = 0;
                     memset(cmd_buf, 0, CMD_BUF_SIZE);
